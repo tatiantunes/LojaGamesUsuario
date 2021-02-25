d058ef4c27d761e6e0987dfbf37f3b0215242b46
tatiantunes <tativive@hotmail.com>
2021-02-25 19:26:17 -0300

    feat(model): create 3 tables in database

---
+++ src/main/java/com/lojagamesUsuario/LojaGamesUsuario/model/Usuario.java
@@ -0,0 +1,79 @@
+package com.lojagamesUsuario.LojaGamesUsuario.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String nome;
+	
+	@NotNull
+	private String email;
+	
+	@NotNull
+	private  int idade;
+	
+	@OneToMany(mappedBy = "usuario",cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Produto> produto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public List<Produto> getProdutos() {
+		return produto;
+	}
+
+	public void setProdutos(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+}
